ea55f221b93bf63c60c1aee90c8632afc701c355
Tom Krok <91782249+trak2022z@users.noreply.github.com>
2022-04-03 17:48:45 +0000

    Exercise 1: Splendid Circles

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//Exercise 1: Splendid Circles
 'use strict';
 
 const express = require('express');
@@ -5,7 +6,12 @@ const app = express();
 
 //Exercise 1: Splendid Circles
 // define endpoint for exercise 1 here
-
+app.get('/math/circle/:r', function(req, res) {
+  let radius = parseFloat(req.params['r']);
+  let area = Math.PI * radius * radius;
+  let circumference = Math.PI * radius * 2;
+  res.json({'area': area, 'circumference': circumference});
+});
 
 // define endpoint for exercise 2 here
 
