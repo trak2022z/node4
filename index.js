a9d217d427b21bc8136bdc2a6f1158f9bedfc321
Tom Krok <91782249+trak2022z@users.noreply.github.com>
2022-04-03 17:53:18 +0000

    Exercise 2: Radical Rectangles

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express();
 
 //Exercise 1: Splendid Circles
 // define endpoint for exercise 1 here
+//https://node4.tomkrok1.repl.co/math/circle/1
 app.get('/math/circle/:r', function(req, res) {
   let radius = parseFloat(req.params['r']);
   let area = Math.PI * radius * radius;
@@ -13,8 +14,16 @@ app.get('/math/circle/:r', function(req, res) {
   res.json({'area': area, 'circumference': circumference});
 });
 
+//Exercise 2: Radical Rectangles
 // define endpoint for exercise 2 here
-
+//https://node4.tomkrok1.repl.co/math/rectangle/5/5
+app.get('/math/rectangle/:width/:height', function(req, res) {
+  let width = parseFloat(req.params['width']);
+  let height = parseFloat(req.params['height']);
+  let area = width * height;
+  let perimeter = (width + height) * 2;
+  res.json({'area': area, 'perimeter': perimeter});
+});
 
 // define endpoint for exercise 3 here
 
