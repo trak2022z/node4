234fb75c69a9770d89a2c9d0fe27f452ec4316ef
TomKrok1 <91782249+trak2022z@users.noreply.github.com>
2022-04-03 17:35:01 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+
+//Exercise 1: Splendid Circles
+// define endpoint for exercise 1 here
+
+
+// define endpoint for exercise 2 here
+
+
+// define endpoint for exercise 3 here
+
+
+// define endpoint for exercise 4 here
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT);
